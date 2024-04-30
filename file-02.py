9b5203ccafe50006a704c55dfad399a515abfe1a
rajeev <rajeevkl07@gmail.com>
2024-04-30 17:42:02 +0530

    comments removed

--- file-02.py
+++ file-02.py
@@ -1,16 +1,20 @@
 
 
-#while True:
-     #name=input("Enter ur name:")
-     #if name!="":
-        #break
-#phone_no="123-456-789"
-#for i in phone_no:
-   #if i=="-":
-      #continue
-   #print(i,end="")
-#for i in range(1,21):
-   # if i==13:
-      # pass
-   # else:
-      #  print(i)
+while True:
+  name=input("Enter ur name:")
+  if name!="":
+    break
+phone_no="123-456-789"
+for i in phone_no:
+  if i=="-":
+    continue
+  print(i,end="")
+for i in range(1,21):
+  if i==13:
+    pass
+  else:
+    print(i)
+
+"""
+this is file 02
+"""
